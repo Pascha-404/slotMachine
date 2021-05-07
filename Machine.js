ec8ca18173ca6b8ff36ac66b45926cda90ed81dd
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-07 11:57:07 +0200

    finished exercise

---
+++ Machine.js
@@ -0,0 +1,17 @@
+class Machine extends React.Component {
+    render() {
+        const {
+            s1,
+            s2,
+            s3
+        } = this.props;
+        const winner = (s1 === s2) && (s2 === s3);
+
+        return (
+        <div className="Machine">
+            <b>{s1}</b> <b>{s2}</b> <b>{s3}</b> 
+            <p> You {winner ? "win!" : "lose!"} </p> 
+            </div>
+        );
+    }
+}
