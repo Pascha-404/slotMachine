64728d58063d017db955f435520a670431de4bcf
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-07 12:07:33 +0200

    added App extension

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const slot = ["🚀", "🍆", "🐼"]
 
+class App extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>Slot Machine</h1>
+            <Machine s1 = "🚀" s2 = "🍆" s3 = "🚀" />
+            <Machine s1 = "🚀" s2 = "🚀" s3 = "🚀" />
+        </div>
+        )
+    }
+}
+
 ReactDOM.render(
-<Machine s1 = "🚀" s2 = "🍆" s3 = "🚀" />,
+<App />,
     document.getElementById("root")
 );
